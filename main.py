3d8cc338476813bbb6a30125467c1907dd247fd6
pepone <pepone@localhost.localdomain>
2025-12-09 12:46:03 +0100

    funciona reseteo de usb

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-Configurar /etc/sudoers para poder ejecutar comandos como super usuario
+import os # https://docs.python.org/3/library/os.html
+import subprocess as sub # https://docs.python.org/3/library/subprocess.html
 
-    # Editor de el fichero de superusuarios
-    sudo visudo
 
-    # Permitir que el usuario 'tu_usuario' ejecute comandos específicos sin password
-    tu_usuario ALL=(ALL) NOPASSWD: /bin/systemctl restart servicio_especifico
-    tu_usuario ALL=(ALL) NOPASSWD: /usr/bin/apt update
-    tu_usuario ALL=(ALL) NOPASSWD: /sbin/reboot
-
-    # O para un script específico
-    tu_usuario ALL=(ALL) NOPASSWD: /ruta/completa/de/tu_script.py
+if __name__=="__main__":
+    # Nombre del dispositivo
+    awus_str = "RTL8812AU"
+    awus_id = None
+        
+    sub.run(["clear"])    
+    
+    lsusb_aux = sub.run(["lsusb"], capture_output=True, text=True, universal_newlines=True) # text=True 
+    
+    lsusb = lsusb_aux.stdout.splitlines()
+    del lsusb_aux
+    
+    
+    for l in lsusb:
+        if l.find(awus_str) >= 0: # busca la línea de la tarjeta wifi
+            list_line = l.split(" ")
+            for i, t in enumerate(list_line): # busca el trozo del literal "ID" y el siguiente es el que contiene el id
+                if list_line[i].lower() == "id":
+                    awus_id = list_line[i+1] # guardamos el ID en memoria
+                    
+    print(awus_id)
+    
+    sub.run(["sudo","usbreset", awus_id])
